a157a4ab5fb23a319c1d79490b21d2ead50d02da
Yukio Rivera <DivinusMessor@users.noreply.github.com>
2022-02-05 01:04:36 -0800

    started resume

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> My app</h1>
+      <h2>Test</h2>
+      <img src={logo} alt="image" />
+    </div>
+  );
+}
+
+export default App;
